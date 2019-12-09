efb25c4021e7a630e222fcff0b4dfd616ec7fe31
Baktiyar Ishenov <b1963826@gmail.com>
2019-12-09 15:24:19 +0600

    App js почти готов

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Joke from "./components/Joke/Joke";
+import Button from "./components/Button/Button";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    jokes: [],
+    defaultCount: 5
+  };
+
+  componentDidMount() {
+    this.getJokes(this.state.defaultCount);
+  };
+
+  getJokes = (count) => {
+    const promises = [];
+    for (let i = 0; i < count; i++) {
+      const result = this.getNewJoke();
+      promises.push(result);
+    }
+
+    Promise.all(promises).then(jokes => {
+      this.setState({jokes});
+    });
+  };
+
+  getNewJoke = () => {
+    return fetch('https://api.chucknorris.io/jokes/random').then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Something wrong with request');
+    }).catch(error=>{
+      console.error(error);
+    });
+  };
+
+  addJoke = () => {
+    this.getNewJoke().then(result => {
+      const jokes = [...this.state.jokes];
+      jokes.push(result);
+      this.setState({jokes});
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.jokes.map(joke => <Joke text={joke.value} key={joke.id} />)}
+        <Button onClick={this.addJoke}>Get new Joke!</Button>
+      </div>
+    );
+  }
 }
 
 export default App;
